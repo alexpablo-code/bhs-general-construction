f67fe9d52c5258ff494f02140c54495512afcea1
Rony Pablo <ronyalexis0@gmail.com>
2023-06-03 11:18:33 -0700

    adding routes and pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
+import Homepage from './pages/Homepage';
+import ServicesPage from './pages/ServicesPage';
+import Gallery from './pages/Gallery';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path='/' element={}/>
+          <Route path='/' element={<Homepage/>}/>
+          <Route path='/services' element={<ServicesPage/>}/>
+          <Route path='/gallery' element={<Gallery/>}/>
         </Routes>
       </div>
     </BrowserRouter>
