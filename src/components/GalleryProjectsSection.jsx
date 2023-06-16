830fda4e9ac4bf2af483eefe9252b00bc2a94088
Rony Pablo <ronyalexis0@gmail.com>
2023-06-16 14:16:29 -0700

    added image sliders to projects

--- src/components/GalleryProjectsSection.jsx
+++ src/components/GalleryProjectsSection.jsx
@@ -1,8 +1,37 @@
 import '../styles/galleryProjectSection.css';
+import {Slide} from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
 const pool = require('../components/images/pool_project.png');
 const patio = require('../components/images/patio_project.png');
+const about1 = require('../components/images/about_1.png');
+const about2 = require('../components/images/about_2.png');
 
 const GalleryProjectsSection = () => {
+
+    const slideImages = [
+        require('../components/images/pool_project.png'),
+        require('../components/images/patio_project.png'),
+        require('../components/images/about_1.png'),
+        require('../components/images/about_2.png'),
+    ];
+    // const imagesUrlExample = [ 
+    //     {
+    //         url: 'https://images.unsplash.com/photo-123234',
+    //         caption: 'First Slide'
+    //     },
+    //     {
+    //         url: 'https://images.unsplash.com/photo-123234',
+    //         caption: 'First Slide'
+    //     },
+    //     {
+    //         url: 'https://images.unsplash.com/photo-123234',
+    //         caption: 'First Slide'
+    //     },
+    //     {
+    //         url: 'https://images.unsplash.com/photo-123234',
+    //         caption: 'First Slide'
+    //     }
+    // ];
     
     return (
         <section>
@@ -12,7 +41,19 @@ const GalleryProjectsSection = () => {
                     <h6>Concrete Repair  Pool Deck</h6>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 </div>
-                <img src={pool} alt="pool-project" className='project-image'/>
+                <div className='slide-container'>
+                    <Slide>
+                        {
+                            slideImages.map((image, index) => (
+                                <div key={index} className='each-slide-effect'>
+                                    <div style={{ 'backgroundImage': `url(${image})` }}>
+                                        <span></span>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </Slide>
+                </div>
             </div>
             <div className="project-box">
                 <div className="project-text-side">
@@ -20,7 +61,19 @@ const GalleryProjectsSection = () => {
                     <h6>Concrete Repair  Pool Deck</h6>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 </div>
-                <img src={patio} alt="patio-project" className='project-image'/>
+                <div className='slide-container'>
+                    <Slide>
+                        {
+                            slideImages.map((image, index) => (
+                                <div key={index} className='each-slide-effect'>
+                                    <div style={{ 'backgroundImage': `url(${image})` }}>
+                                        <span></span>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </Slide>
+                </div>
             </div>
             <div className="project-box">
                 <div className="project-text-side">
@@ -28,7 +81,19 @@ const GalleryProjectsSection = () => {
                     <h6>Concrete Repair  Pool Deck</h6>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 </div>
-                <img src={pool} alt="pool-project" className='project-image'/>
+                <div className='slide-container'>
+                    <Slide>
+                        {
+                            slideImages.map((image, index) => (
+                                <div key={index} className='each-slide-effect'>
+                                    <div style={{ 'backgroundImage': `url(${image})` }}>
+                                        <span></span>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </Slide>
+                </div>
             </div>
             <div className="project-box">
                 <div className="project-text-side">
@@ -36,7 +101,19 @@ const GalleryProjectsSection = () => {
                     <h6>Concrete Repair  Pool Deck</h6>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 </div>
-                <img src={patio} alt="patio-project" className='project-image'/>
+                <div className='slide-container'>
+                    <Slide>
+                        {
+                            slideImages.map((image, index) => (
+                                <div key={index} className='each-slide-effect'>
+                                    <div style={{ 'backgroundImage': `url(${image})` }}>
+                                        <span></span>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </Slide>
+                </div>
             </div>
         </section>
     );
