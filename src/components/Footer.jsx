607bd4a1dca51defa2c40b2c9eecb8bda13d5741
Rony Pablo <ronyalexis@gmail.com>
2023-08-28 10:01:13 -0700

    ...

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import '../styles/footer.css'
-import { useNavigate } from 'react-router-dom';
-const logo = require('../components/images/blk_font_logo.png');
+import React from "react";
+import "../styles/footer.css";
+import { useNavigate } from "react-router-dom";
+const logo = require("../components/images/blk_font_logo.png");
 
 const Footer = () => {
-    const navigate = useNavigate();
-    return (
-        <footer>
-            <img src={logo} alt="site-logo" className="footer-logo" onClick={() => navigate('/')}/>
-            <p>©️ 2023 BHS General Construction. All rights reserved</p>
-            <p>Website by: <a href='https://www.instagram.com/the.alexpablo'>Alex Pablo</a></p>
-        </footer>
-    );
-}
+  const navigate = useNavigate();
+  return (
+    <footer>
+      <img
+        src={logo}
+        alt="site-logo"
+        className="footer-logo"
+        onClick={() => navigate("/")}
+      />
+      <p>©️ 2023 BHS General Construction. All rights reserved</p>
+      <p>
+        Website by:{" "}
+        <a href="https://www.instagram.com/the.alexpablo">Alex Pablo</a>
+      </p>
+    </footer>
+  );
+};
 
 export default Footer;
